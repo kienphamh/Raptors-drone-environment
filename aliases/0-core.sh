a6c743a3b81c6aaad32d69f0bfe681a8109b255b
kienphamh <phamhuuduckien@gmail.com>
2026-02-06 19:33:43 +0100

    fix: correct FCU_URL default value in core environment configuration

--- aliases/0-core.sh
+++ aliases/0-core.sh
@@ -2,8 +2,7 @@
 # Core environment variables and configuration
 
 export ARDUPILOT_HOME=${ARDUPILOT_HOME:-/opt/ardupilot/ArduCopter}
-export GAZEBO_WORLD=${GAZEBO_WORLD:-~/ardupilot_gazebo/iris_runway.sdf}
-export FCU_URL=${FCU_URL:-udp://:14550@127.0.0.1:14557}
+export FCU_URL=${FCU_URL:-udp://:14550@127.0.0.1:14550@}
 
 # Workspace shortcuts
 alias ws='cd /workspace'
