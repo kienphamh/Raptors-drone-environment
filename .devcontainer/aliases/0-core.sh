3d96a4fcc2466052178517d1207b1f397a10e524
kienphamh <phamhuuduckien@gmail.com>
2026-05-15 17:14:04 +0200

    Update environment variables in 0-core.sh for correct ArduPilot paths

--- .devcontainer/aliases/0-core.sh
+++ .devcontainer/aliases/0-core.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Core environment variables and configuration
 
-export ARDUPILOT_HOME=${ARDUPILOT_HOME:-/opt/ardupilot/ArduCopter}
-export FCU_URL=${FCU_URL:-udp://:14550@}
+export ARDUPILOT_HOME=${ARDUPILOT_HOME:-/opt/ardupilot}
+export FCU_URL=${FCU_URL:-udp://:14551@}
 export GZ_VERSION=harmonic
 export GZ_SIM_SYSTEM_PLUGIN_PATH=/usr/local/lib/ardupilot_gazebo
 export GZ_SIM_RESOURCE_PATH=/opt/ardupilot_gazebo/models:/opt/ardupilot_gazebo/worlds
